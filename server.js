99a2a5d8f14a2c1b7f18db274e2f34fe40d7550f
shivikagarg999 <shivikagarg91@gmail.com>
2025-06-16 11:10:28 +0530

    change port

--- server.js
+++ server.js
@@ -2,17 +2,13 @@ const http = require('http');
 const os = require('os');
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
-// const { connectRedis } = require('./src/config/redis');
 const connectSocket = require('./src/config/socket');
-
 
 const server = http.createServer(app);
 connectSocket(server);
 connectDB();
-// connectRedis();
 
-
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
     const networkInterfaces = os.networkInterfaces();
@@ -20,5 +16,5 @@ server.listen(port, () => {
         .flat()
         .filter((details) => details.family === "IPv4" && !details.internal)
         .map((details) => details.address)[0];
-    console.log('✅ Backend running');
+    console.log(`✅ Backend running on port ${port}`);
 });
